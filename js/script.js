4865d8b0f23db8c6f48d5d9ed3f126bcd683232c
Hernando Abella <hernandoabella@gmail.com>
2022-02-11 16:49:12 -0500

    agregando estilos a todos los elementos

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// variables
+
+let numeroCarrito = document.querySelector('.numeroCarrito');
+let mas = document.querySelector('.mas');
+let menos = document.querySelector('.menos');
+let precio = document.querySelctor('.precio');
+
+const mas = () => {
+    
+}
+
+const menos = () => {
+    
+}
