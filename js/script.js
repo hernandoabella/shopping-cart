397e5a20855c58a64a3fa01bd7082cdf68f1751a
Hernando Abella <hernandoabella@gmail.com>
2022-02-11 18:15:37 -0500

    mejoras en los botones

--- js/script.js
+++ js/script.js
@@ -1,17 +1,21 @@
 // variables
 
-let numeroCarrito = document.querySelectorAll('.numeroCarrito');
+let numeroCarrito = document.querySelector('.numeroCarrito');
 let btnPlus = document.querySelector('.mas');
 let btnLess = document.querySelector('.menos');
-let precio = document.querySelectorAll('.precio');
-let counter = document.querySelectorAll('placehold__number').innerHTML;
+let precio = document.querySelector('.precio');
+let counter = document.querySelector('.placehold__number');
 
 const mas = () => {
-    
+    +numeroCarrito.innerHTML ++;
 }
 
 const menos = () => {
-    
+    counter.innerHTML--;
+}
+
+const checkout = () => {
+
 }
 
 btnPlus.addEventListener('click', mas)
